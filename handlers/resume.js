21e5318b94972f681e6549a0376727eaf60f1c41
Aaron Macken <aaron.a.macken@gmail.com>
2019-10-27 17:34:16 -0500

    Updated resume file and included a new PDF in the public folder

--- handlers/resume.js
+++ handlers/resume.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 exports.sendResume = (req, res, next) => {
-    const file = path.join(__dirname,'../public/AaronResume.pdf');
+    const file = path.join(__dirname,'../public/AaronMackenResume.pdf');
     res.download(file, function(err) {
         if(err) {
             console.log("Error");
