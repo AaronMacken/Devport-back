c73b33ccc6b5f7ee643c36567313f6ad0db955cd
Aaron Macken <aaron.a.macken@gmail.com>
2019-11-03 22:24:53 -0800

    change node mailer service

--- handlers/nodeMailer.js
+++ handlers/nodeMailer.js
@@ -4,11 +4,16 @@ require("dotenv").load;
 exports.sendMail = (req, res, next) => {
   // node mailer set up
   let transport = nodemailer.createTransport({
-    service: "gmail",
+    host: 'smtp.mail.yahoo.com',
+    port: 465,
+    service: "yahoo",
+    secure: false,
     auth: {
       user: process.env.SECRET_U,
       pass: process.env.SECRET_P
-    }
+    },
+    debug: false,
+    logger: true
   });
 
   // send email via node mailer
@@ -18,7 +23,7 @@ exports.sendMail = (req, res, next) => {
     subject: `Devport Email from ${req.body.messageData.name} - ${req.body.messageData.email} at ${req.body.messageData.company}`, // Subject line
     text: req.body.messageData.message // Plain text body
   };
-  transport.sendMail(message, function(err, info) {
+  transport.sendMail(message, function (err, info) {
     if (err) {
       console.log(err);
     } else {
