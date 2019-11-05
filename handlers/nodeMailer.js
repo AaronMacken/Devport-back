bcd2d6af398b6cd63ad7c75cf60140c5fd61af14
Aaron Macken <aaron.a.macken@gmail.com>
2019-11-05 12:37:02 -0800

    implement Oauth2 for nodemailer with gmail

--- handlers/nodeMailer.js
+++ handlers/nodeMailer.js
@@ -1,15 +1,31 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 require("dotenv").load;
+
+// oath client setup
+const oauth2Client = new OAuth2(
+  `${process.env.SECRET_CID}`,
+  `${process.env.SECRET_CS}`,
+  "https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({
+  refresh_token: `${process.env.SECRET_RT}`
+});
+const accessToken = oauth2Client.getAccessToken()
 
 exports.sendMail = (req, res, next) => {
   // node mailer set up
   let transport = nodemailer.createTransport({
-    host: 'smtp.mail.yahoo.com',
-    port: 587,
-    service: "yahoo",
+    service: "gmail",
     auth: {
-      user: process.env.SECRET_U,
-      pass: process.env.SECRET_P
+      type: "OAuth2",
+      user: "aaron.a.macken@gmail.com",
+      clientId: `${process.env.SECRET_CID}`,
+      clientSecret: `${process.env.SECRET_CS}`,
+      refreshToken: `${process.env.SECRET_RT}`,
+      accessToken: accessToken
     },
     debug: false,
     logger: true
@@ -17,8 +33,8 @@ exports.sendMail = (req, res, next) => {
 
   // send email via node mailer
   const message = {
-    from: "aaron_macken@yahoo.com", // Sender address
-    to: "aaron_macken@yahoo.com", // List of recipients
+    from: "aaron.a.macken@gmail.com", // Sender address
+    to: "aaron.a.macken@gmail.com", // List of recipients
     subject: `Devport Email from ${req.body.messageData.name} - ${req.body.messageData.email} at ${req.body.messageData.company}`, // Subject line
     text: req.body.messageData.message // Plain text body
   };
